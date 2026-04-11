26c97927300a72347b0e875c275ac8ca0e93d818
robobogus25-lang <robobogus25@gmail.com>
2026-04-11 19:12:02 +0300

    4234

--- static/script.js
+++ static/script.js
@@ -1,12 +1,1 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-
-const pathToIndex = path.join(__dirname, 'static', 'index.html');
-const indexHtmlFile = fs.readFileSync(pathToIndex);
-
-const server = http.createServer((req, res) => {
-
-});
-
-server.listen(3000);
+alert('Chat')
