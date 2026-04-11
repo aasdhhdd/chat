9909bd5c47d3f90f25d610f1565dd2a0ae1d2e60
robobogus25-lang <robobogus25@gmail.com>
2026-04-11 19:03:23 +0300

    25

--- static/script.js
+++ static/script.js
@@ -1,4 +1,9 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const pathToIndex = path.join(__dirname, 'static', 'index.html');
+const indexHtmlFile = fs.readFileSync(pathToIndex);
 
 const server = http.createServer((req, res) => {
 
