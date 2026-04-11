7899b7165343d04d45b2ae5d4c725c15825dc6c6
robobogus25-lang <robobogus25@gmail.com>
2026-04-11 19:26:10 +0300

    123

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const path = require('path');
 
 const pathToIndex = path.join(__dirname, 'static', 'index.html');
 const indexHtmlFile = fs.readFileSync(pathToIndex);
+const pathToScript = path.join(__dirname, 'static', 'script.js');
+const scriptJsFile = fs.readFileSync(pathToScript);
+const pathToStyle = path.join(__dirname, 'static', 'style.css');
+const styleCssFile = fs.readFileSync(pathToStyle);
+
 
 const server = http.createServer((req, res) => {
     if(req.url === '/') {
