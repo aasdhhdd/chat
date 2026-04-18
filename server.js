0feebd07207e9cbca7c3c8f5b4bd74c65ef18a9c
robobogus25-lang <robobogus25@gmail.com>
2026-04-18 19:29:06 +0300

    35345

--- server.js
+++ server.js
@@ -18,3 +18,13 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) =>  {
+    console.log('a user connected. id - ' + socket.id) ;
+    socket.on('new_message', (message) => {
+        console.log(message);
+    });
+});
--- static/script.js
+++ static/script.js
@@ -1,1 +1,9 @@
-alert('Chat')
+const socket = io();
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('new_message', input.value);
+    input.value = '';
+  }
+});
